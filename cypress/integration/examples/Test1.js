0f96c787e5c252de0009531132232eb41309f0a8
viktorija-verebes <viki.verebes90@gmail.com>
2024-09-11 08:49:09 +0200

    get second element

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -11,5 +11,7 @@ describe("My First Test", () => {
 
     // Parent child chaining
     cy.get(".products").find(".product").should("have.length", 4);
+
+    cy.get(".products").find(".product").eq(2);
   });
 });
