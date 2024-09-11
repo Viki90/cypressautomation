b5562c2f22e33aa188490c1be5f5d49d06e43843
viktorija-verebes <viki.verebes90@gmail.com>
2024-09-11 08:42:59 +0200

    parent child chaining

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -8,5 +8,8 @@ describe("My First Test", () => {
     cy.wait(2000);
 
     cy.get(".product:visible").should("have.length", 4);
+
+    // Parent child chaining
+    cy.get(".products").find(".product").should("have.length", 4);
   });
 });
