33543ef7bb0a097914f78c4bb202f19d02ed55a7
viktorija-verebes <viki.verebes90@gmail.com>
2024-09-11 08:57:22 +0200

    click on the add to cart button

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -12,6 +12,6 @@ describe("My First Test", () => {
     // Parent child chaining
     cy.get(".products").find(".product").should("have.length", 4);
 
-    cy.get(".products").find(".product").eq(2);
+    cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
   });
 });
