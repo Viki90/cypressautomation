3743eeb04650ab194535317f6b2446fb02fe25f9
viktorija-verebes <viki.verebes90@gmail.com>
2024-09-18 22:08:42 +0200

    mouse hover events

--- cypress/integration/examples/Test8.js
+++ cypress/integration/examples/Test8.js
@@ -4,8 +4,8 @@
 describe("My 8 Test", () => {
   it("Does not do much!", () => {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-    cy.get(".mouse-hover-content").invoke("show");
-    cy.contains("Top").click();
+    //cy.get("div.mouse-hover-content").invoke("show");
+    cy.contains("Top").click({ force: true });
     cy.url().should("include", "top");
   });
 });
