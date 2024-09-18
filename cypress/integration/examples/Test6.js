8a6e8edf3b1b5624aab4b90bfbfb24c14cb021ef
viktorija-verebes <viki.verebes90@gmail.com>
2024-09-18 21:17:45 +0200

    check if have the expected title

--- cypress/integration/examples/Test6.js
+++ cypress/integration/examples/Test6.js
@@ -8,6 +8,7 @@ describe("My 6 Test", () => {
 
     cy.origin("https://www.qaclickacademy.com/", () => {
       cy.get("#navbarSupportedContent a[href*='about']").click();
+      cy.get(".mt-50 h2").should("contain", "Welcome to QAClick Academy ");
     });
   });
 });
