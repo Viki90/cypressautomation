2f780ef7a1110aa7e724beef9f64379dce226ffb
viktorija-verebes <viki.verebes90@gmail.com>
2024-09-19 18:48:08 +0200

    Adding a wait ensures that the page has enough time to load and stabilize before Cypress continues with the next commands. This can help prevent issues where elements are not yet available or are being updated asynchronously.

--- cypress/integration/examples/Test10.js
+++ cypress/integration/examples/Test10.js
@@ -10,10 +10,21 @@ describe("My 10 Test", () => {
     // Check if iframe is loaded
     cy.iframe().should("exist");
 
-    // Check if the mentorship link exists
-    cy.iframe().find('a[href*="mentorship"]').should("exist").eq(0).click();
+    // Check if the mentorship link exists and click it
+    cy.iframe()
+      .find('a[href*="mentorship"]')
+      .should("exist")
+      .eq(0)
+      .as("mentorshipLink");
+    cy.get("@mentorshipLink").click();
+
+    // Wait for the page to load
+    cy.wait(2000);
+
+    // Re-query the iframe to ensure the DOM is up-to-date
+    cy.frameLoaded("#courses-iframe");
 
     // Check if the pricing titles are present
-    // cy.iframe().find('h1[class*="pricing-title"]').should("have.length", 2);
+    cy.iframe().find('h1[class*="pricing-title"]').should("have.length", 2);
   });
 });
