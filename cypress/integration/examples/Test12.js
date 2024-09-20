266c8744f3865741366ae660e2882d110a12cb04
viktorija-verebes <viki.verebes90@gmail.com>
2024-09-20 13:59:24 +0200

    select statict data

--- cypress/integration/examples/Test12.js
+++ cypress/integration/examples/Test12.js
@@ -5,7 +5,10 @@ describe("My 12 Test", () => {
 
   it("Does not do much!", () => {
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
-    cy.get(":nth-child(1) > .form-control").should("be.visible");
+    //cy.get(":nth-child(1) > .form-control").should("be.visible");
+    cy.get('input[name="name"]:nth-child(2)').should("be.visible");
     cy.get(":nth-child(1) > .form-control").type("Bob");
+
+    cy.get("select").select("Female");
   });
 });
