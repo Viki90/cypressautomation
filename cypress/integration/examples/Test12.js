555f5cf13c9802123824b0d06150ed1b0f7b6413
viktorija-verebes <viki.verebes90@gmail.com>
2024-09-20 16:03:33 +0200

    fixture and select

--- cypress/integration/examples/Test12.js
+++ cypress/integration/examples/Test12.js
@@ -1,14 +1,31 @@
 /// <reference types="Cypress" />
 
 describe("My 12 Test", () => {
-  before(function () {});
+  before(function () {
+    cy.fixture("example").then((data) => {
+      this.data = data;
+      cy.log("Fixture data:", this.data); // Log fixture data
+    });
+  });
 
-  it("Does not do much!", () => {
+  it("Does not do much!", function () {
     cy.visit("https://rahulshettyacademy.com/angularpractice/");
-    //cy.get(":nth-child(1) > .form-control").should("be.visible");
     cy.get('input[name="name"]:nth-child(2)').should("be.visible");
-    cy.get(":nth-child(1) > .form-control").type("Bob");
+    cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
 
-    cy.get("select").select("Female");
+    // Ensure the select element is visible and log its options
+    cy.get("select")
+      .should("be.visible")
+      .then(($select) => {
+        const options = $select
+          .find("option")
+          .map((i, el) => el.value)
+          .get();
+        cy.log("Available options:", options);
+      });
+
+    //cy.get("select").select(this.data.gender);
+    cy.get("select").contains("option", this.data.gender).should("exist");
+    cy.get("select").select(this.data.gender);
   });
 });
