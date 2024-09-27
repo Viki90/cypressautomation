082fd25e1174d32ed1ec63c7d8500573015c8f06
viktorija-verebes <viki.verebes90@gmail.com>
2024-09-27 08:24:23 +0200

    custom commands

--- cypress/integration/examples/Test13.js
+++ cypress/integration/examples/Test13.js
@@ -39,10 +39,6 @@ describe("My 12 Test", () => {
 
     cy.get(":nth-child(2) > .nav-link").click();
 
-    cy.get("h4.card-title").each(($el, index, $list) => {
-      if ($el.text().includes("Blackberry")) {
-        cy.get("button.btn.btn-info").eq(index).click();
-      }
-    });
+    cy.selectProduct("Blackberry");
   });
 });
