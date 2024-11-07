6aac82cbcda0614e1f3868c31414ae351117f06d
viktorija-verebes <viki.verebes90@gmail.com>
2024-11-07 17:40:42 +0100

    adding some changes for reporter and projectID

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,24 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    useInlineDiffs: true,
+    embeddedScreenshots: true,
+    reportDir: "cypress/results",
+    reportFilename: "my-report.html",
+    overwrite: true,
+    html: true,
+    json: true,
+  },
+  projectId: "uzqxkb",
   defaultCommandTimeout: 6000,
   env: {
     url: "https://rahulshettyacademy.com",
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
     specPattern: "cypress/integration/examples/*.js",
   },
