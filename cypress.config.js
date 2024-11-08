6ba4360cfb393a1eb3e1edd1520892da9244c050
viktorija-verebes <viki.verebes90@gmail.com>
2024-11-08 20:53:10 +0100

    adding more setup to cypres.config.js and package.json files

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,26 @@
 const { defineConfig } = require("cypress");
 
+const browserify = require("@cypress/browserify-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprendTransformerToOptions,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    browserify(preprendTransformerToOptions(config, browserify.defaultOptions))
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
@@ -30,6 +51,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
     },
-    specPattern: "cypress/integration/examples/*.js",
+    // specPattern: "cypress/integration/examples/*.js",
+    specPattern: "cypress/integration/examples/BDD/*.feature",
   },
 });
